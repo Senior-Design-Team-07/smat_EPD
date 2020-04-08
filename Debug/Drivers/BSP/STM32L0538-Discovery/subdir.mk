4c0f4e72a1cd21867d833aa4a9b477b8bb710226
Jordan Loeser <loeser.jordan@gmail.com>
2020-04-08 16:30:08 -0400

    initial commit

---
+++ Debug/Drivers/BSP/STM32L0538-Discovery/subdir.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/STM32L0538-Discovery/font12epd.c \
+../Drivers/BSP/STM32L0538-Discovery/font16epd.c \
+../Drivers/BSP/STM32L0538-Discovery/font20epd.c \
+../Drivers/BSP/STM32L0538-Discovery/font8epd.c \
+../Drivers/BSP/STM32L0538-Discovery/stm32l0538_discovery.c \
+../Drivers/BSP/STM32L0538-Discovery/stm32l0538_discovery_epd.c 
+
+OBJS += \
+./Drivers/BSP/STM32L0538-Discovery/font12epd.o \
+./Drivers/BSP/STM32L0538-Discovery/font16epd.o \
+./Drivers/BSP/STM32L0538-Discovery/font20epd.o \
+./Drivers/BSP/STM32L0538-Discovery/font8epd.o \
+./Drivers/BSP/STM32L0538-Discovery/stm32l0538_discovery.o \
+./Drivers/BSP/STM32L0538-Discovery/stm32l0538_discovery_epd.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32L0538-Discovery/font12epd.d \
+./Drivers/BSP/STM32L0538-Discovery/font16epd.d \
+./Drivers/BSP/STM32L0538-Discovery/font20epd.d \
+./Drivers/BSP/STM32L0538-Discovery/font8epd.d \
+./Drivers/BSP/STM32L0538-Discovery/stm32l0538_discovery.d \
+./Drivers/BSP/STM32L0538-Discovery/stm32l0538_discovery_epd.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32L0538-Discovery/%.o: ../Drivers/BSP/STM32L0538-Discovery/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32L053xx -I"C:/Users/Marco/Documents/School/ECE 477/EPD_test/Inc" -I"C:/Users/Marco/Documents/School/ECE 477/EPD_test/Drivers/STM32L0xx_HAL_Driver/Inc" -I"C:/Users/Marco/Documents/School/ECE 477/EPD_test/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Marco/Documents/School/ECE 477/EPD_test/Drivers/CMSIS/Device/ST/STM32L0xx/Include" -I"C:/Users/Marco/Documents/School/ECE 477/EPD_test/Drivers/CMSIS/Include" -I"C:/Users/Marco/Documents/School/ECE 477/EPD_test/Drivers/BSP/STM32L0538-Discovery"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/sources.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Drivers/BSP/Components/gde021a1 \
+Drivers/BSP/Components/hx8347d \
+Drivers/BSP/Components/mfxstm32l152 \
+Drivers/BSP/Components/st7735 \
+Drivers/BSP/Components/stlm75 \
+Drivers/BSP/STM32L0538-Discovery \
+Drivers/STM32L0xx_HAL_Driver/Src \
+Src \
+startup \
+
